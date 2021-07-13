449418533aa70c6894f91898b6864946a6ec6c0e
stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2021-07-13 09:08:52 -0400

    feat: test peerid in ipfs connection list

--- test/connect.test.js
+++ test/connect.test.js
@@ -1,16 +1,26 @@
 const { IpfsDaemon } = require('@ceramicnetwork/ipfs-daemon')
 
-const CERAMIC_NETWORK = 'dev-unstable'
-const { MULTIADDRESS } = process.env
+const { CERAMIC_NETWORK, MULTIADDRESS } = process.env
+
+const peerId = MULTIADDRESS.split('/').pop()
 
 describe('@ceramicnetwork/ipfs-daemon', () => {
-  test('starts successfully with custom bootstrap list', async () => {
+  test('connects successfully with custom bootstrap list', async () => {
+    const consoleSpy = jest.spyOn(console, 'log')
+
     const ipfsDaemon = await IpfsDaemon.create({
       useCentralizedPeerDiscovery: true,
       ceramicNetwork: CERAMIC_NETWORK,
       ipfsBootstrap: [MULTIADDRESS]
     })
     await ipfsDaemon.start()
+
+    const result = consoleSpy.mock.calls.some((log) => {
+      return log[0].includes(peerId)
+    })
+
+    expect(result).toBeTruthy()
+
     await ipfsDaemon.stop()
   })
 })
