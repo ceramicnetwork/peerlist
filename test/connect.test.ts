f5b76ddf36e61afdd1f8619748f9b9ac616395a2
stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2021-08-05 14:47:54 -0400

    fix: do not share ipfs daemon btwn test

--- test/connect.test.ts
+++ test/connect.test.ts
@@ -18,15 +18,11 @@ const peerlistByNetwork: Record<string, string[]> = {
 const tempFilePath = () => `/tmp/jest/peerlist/connect.test.ts/${Date.now()}`
 
 describe('IpfsDaemon', () => {
-    let ipfsDaemon;
-    afterEach(async () => {
-        await ipfsDaemon.stop()
-    })
     test('fails to connect to invalid bootstrap list', async () => {
         const consoleSpy = jest.spyOn(console, 'log')
 
         const peerlist = ['not.a.multiaddress']
-        ipfsDaemon = await createIpfsDaemon(peerlist)
+        const ipfsDaemon = await createIpfsDaemon(peerlist)
         await ipfsDaemon.start()
 
         for (const multiaddress of peerlist) {
@@ -36,12 +32,13 @@ describe('IpfsDaemon', () => {
             })
             expect(result).toBeTruthy()
         }
+        await ipfsDaemon.stop()
     })
     test('connects successfully to valid bootstrap list', async () => {
         const consoleSpy = jest.spyOn(console, 'log')
 
         const peerlist = peerlistByNetwork[ceramicNetwork]
-        ipfsDaemon = await createIpfsDaemon(peerlist)
+        const ipfsDaemon = await createIpfsDaemon(peerlist)
         await ipfsDaemon.start()
 
         for (const multiaddress of peerlist) {
@@ -51,6 +48,7 @@ describe('IpfsDaemon', () => {
             })
             expect(result).toBeTruthy()
         }
+        await ipfsDaemon.stop()
     })
 })
 
