492cdc48b680302f97884d8a72dca33249d7349b
stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2021-08-04 22:01:30 -0400

    feat: clean up test commands and remove jest resolver

--- test/jest.config.js
+++ test/jest.config.js
@@ -2,7 +2,6 @@
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/en/configuration.html
  */
-const { defaults } = require('jest-config')
 
 module.exports = {
   preset: "ts-jest",
